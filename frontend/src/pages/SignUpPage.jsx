f4058d5d9bb15702832382864003a9dd4e86bad8
abderrhim <132024050+abderrahim40k4@users.noreply.github.com>
2024-09-08 11:17:22 +0200

    login UI

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import { motion } from "framer-motion";
+import { Loader, Lock, Mail, User } from "lucide-react";
+
 import {
     Card,
     CardContent,
@@ -7,25 +10,108 @@ import {
     CardHeader,
     CardTitle,
 } from "@/components/ui/card"
-import {InputFaild} from '@/components/InputFaild.jsx'
+import InputFaild from '@/components/InputFaild.jsx'
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+
+import { Button } from "@/components/ui/button"
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form"
+
+const formSchema = z.object({
+    username: z.string().min(2, {
+        message: "Username must be at least 2 characters.",
+    }),
+    useremail: z.string().min(2, {
+        message: "useremail must be at least 2 characters.",
+    }),
+    userpassword: z.string().min(2, {
+        message: "userpassword must be at least 2 characters.",
+    }),
+})
+
 
 
 const SignUpPage = () => {
+    const form = useForm({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            username: "",
+            useremail: "",
+            userpasssword: "",
+        },
+    })
     return (
-        <div>
-            <Card className="w-[400px]">
-                <CardHeader>
-                    <CardTitle>Card Title</CardTitle>
-                    <CardDescription>Card Description</CardDescription>
+        <motion.div
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5 }}
+            className='max-w-md w-full bg-gray-800 bg-opacity-10 backdrop-filter backdrop-blur-xl rounded-2xl shadow-xl 
+			overflow-hidden'
+        >
+            <Card className="max-w-md w-full bg-gray-800 bg-opacity-10 backdrop-filter backdrop-blur-xl rounded-2xl shadow-xl 
+			overflow-hidden'
+">
+                <CardHeader className="inline-flex justify-center items-center text-center w-full">
+                    <CardTitle className="text-center w-full text-4xl text-blue-500 font-bold">LOG IN</CardTitle>
                 </CardHeader>
                 <CardContent>
-                <InputFaild/>
+
+                    <Form {...form}>
+                        <form  className="space-y-8">
+                            <FormField
+                                control={form.control}
+                                name="username"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormControl>
+                                            <InputFaild placeholder={"User Email"} icon={Mail}></InputFaild>
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                            <FormField
+                                control={form.control}
+                                name="useremail"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormControl>
+                                            <InputFaild placeholder={"User Name"} icon={User}></InputFaild>
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                            <FormField
+                                control={form.control}
+                                name="userpassword"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormControl>
+                                            <InputFaild placeholder={"User Password"} icon={Lock}></InputFaild>
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                            <Button type="submit" className="w-full bg-blue-500">Submit</Button>
+                        </form>
+                    </Form>
+
                 </CardContent>
                 <CardFooter>
-                    <p>Card Footer</p>
                 </CardFooter>
             </Card>
-        </div>
+        </motion.div>
     )
 }
 
