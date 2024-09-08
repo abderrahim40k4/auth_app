09b7b5aed971d5866cf8ea6228545a114cfbcf76
abderrhim <132024050+abderrahim40k4@users.noreply.github.com>
2024-09-08 15:35:50 +0200

    Update otpVefiction component

--- frontend/src/components/otpVerfication.jsx
+++ frontend/src/components/otpVerfication.jsx
@@ -6,32 +6,47 @@ import {
     InputOTPSlot,
 } from "@/components/ui/input-otp"
 import { Button } from '@/components/ui/button'
-const OTPVerfication = () => {
-  return (
-    <div>
-        <div>
-            <InputOTP maxLength={10}>
-            <InputOTPGroup>
-                <InputOTPSlot index={0} />
-                <InputOTPSlot index={1} />
-                <InputOTPSlot index={2} />
-                <InputOTPSlot index={3} />
-                <InputOTPSlot index={4} />
-            </InputOTPGroup>
-            <InputOTPSeparator />
-            <InputOTPGroup>
-                <InputOTPSlot index={5} />
-                <InputOTPSlot index={6} />
-                <InputOTPSlot index={7} />
-                <InputOTPSlot index={8} />
-                <InputOTPSlot index={9} />
-            </InputOTPGroup>
-        </InputOTP>
-        <Button className="w-full bg-blue-500 bottom-10 my-11" type="submit"  >Submit</Button>
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
 
+const OTPVerfication = () => {
+    return (
+        <div>
+            <div>
+                <Card className="max-w-md w-full bg-gray-800 bg-opacity-10 backdrop-filter backdrop-blur-xl rounded-2xl shadow-xl overflow-hidden">
+                    <CardHeader className="inline-flex justify-center items-center text-center w-full">
+                        <CardTitle className="text-center w-full text-4xl text-blue-500 font-bold">Card Title</CardTitle>
+                        <CardDescription className="w-full font-bold text-gray-300 text-sm ">Enter the code verfiction you recived from Email</CardDescription>
+                    </CardHeader>
+                    <CardContent>
+                        <InputOTP maxLength={8}>
+                            <InputOTPGroup>
+                                <InputOTPSlot index={0} />
+                                <InputOTPSlot index={1} />
+                                <InputOTPSlot index={2} />
+                                <InputOTPSlot index={3} />
+                            </InputOTPGroup>
+                            <InputOTPSeparator />
+                            <InputOTPGroup>
+                                <InputOTPSlot index={4} />
+                                <InputOTPSlot index={5} />
+                                <InputOTPSlot index={6} />
+                                <InputOTPSlot index={7} />
+                            </InputOTPGroup>
+                        </InputOTP>
+                        <Button className="w-full bg-blue-500 bottom-10 my-11" type="submit"  >Submit</Button>
+                    </CardContent>
+                </Card>
+
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default OTPVerfication
